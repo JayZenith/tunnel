8674724c685dd5aa40327874771dd31d0b4ec041
Jay Zenith <jayzenith248@gmail.com>
2025-03-21 21:35:29 -0700

    test

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jayz/Desktop/tunnel/tests
+# Build directory: /home/jayz/Desktop/tunnel/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/jayz/Desktop/tunnel/build/tests/my_tests[1]_include.cmake")
+add_test(my_tests "/home/jayz/Desktop/tunnel/build/tests/my_tests")
+set_tests_properties(my_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/jayz/Desktop/tunnel/tests/CMakeLists.txt;16;add_test;/home/jayz/Desktop/tunnel/tests/CMakeLists.txt;0;")
